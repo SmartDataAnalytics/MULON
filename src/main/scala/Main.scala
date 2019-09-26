c155636f258d738dbb2771dcbaefe1e24c99adad
shmkhaled <shmkhaled@gmail.com>
2019-09-26 13:25:52 +0200

    mvn dependencies

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+//from flair.data import Sentence
+//from flair.models import SequenceTagger
+//from segtok.segmenter import split_single
+//from flair.data import segtok_tokenizer
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello")
+  }
+}
